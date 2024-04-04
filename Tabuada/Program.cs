84ed2d90c62dd2c42b1d3705a312a5ebe81aae76
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:14:20 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int N;
+                N = int.Parse(Console.ReadLine());  
+
+                for (int i = 1; i <=10; i++) {
+                    int resultado = i * N;
+                    Console.WriteLine(i + " x " + N + " = " + resultado);
+                }
+            }
+        }
+    }
+}
